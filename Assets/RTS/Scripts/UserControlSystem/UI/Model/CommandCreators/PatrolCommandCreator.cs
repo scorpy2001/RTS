43ebb397cb11ad586c2e21be3de1e0a9026fd47e
Александр Биньковский <scorpy2001@gmail.com>
2021-06-02 22:39:00 +0300

    Async / Await Patrol Command.

--- Assets/RTS/Scripts/UserControlSystem/UI/Model/CommandCreators/PatrolCommandCreator.cs
+++ Assets/RTS/Scripts/UserControlSystem/UI/Model/CommandCreators/PatrolCommandCreator.cs
@@ -1,40 +1,14 @@
-using System;
 using RTS.Abstractions;
 using RTS.UserControlSystem.Model;
-using RTS.Utils;
 using UnityEngine;
 using Zenject;
 
 namespace RTS.UserControlSystem.UiModel
 {
-    public class PatrolCommandCreator : CommandCreatorBase<IPatrolCommand>
+    public class PatrolCommandCreator : CancellableCommandCreatorBase<IPatrolCommand, Vector3>
     {
-        [Inject] private AssetsContext _context;
         [Inject] private SelectableValueModel _selectable;
 
-        private Action<IPatrolCommand> _creationCallback;
-        
-        [Inject]
-        private void Init(Vector3Value groundClick)
-        {
-            groundClick.OnChange += OnChangeValue;
-        }
-
-        private void OnChangeValue(Vector3 groundClick)
-        {
-            _creationCallback?.Invoke(_context.Inject(new PatrolCommand(_selectable.CurrentValue.Transform.position, groundClick)));
-            _creationCallback = null;
-        }
-
-        protected override void SpecificCommandCreation(Action<IPatrolCommand> creationCallback)
-        {
-            _creationCallback = creationCallback;
-        }
-
-        public override void ProcessCancel()
-        {
-            base.ProcessCancel(); 
-            _creationCallback = null;
-        }
+        protected override IPatrolCommand CreateCommand(Vector3 argument) => new PatrolCommand(_selectable.CurrentValue.Transform.position, argument);
     }
 }
